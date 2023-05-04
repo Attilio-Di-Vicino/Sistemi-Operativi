42ca9a7e74cd7bf550335018d5b7ff885a578c9c
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-05-04 17:58:42 +0200

    alias

--- alias/set-alias.sh
+++ alias/set-alias.sh
@@ -1,14 +1,25 @@
 # chmod +x set-alias.sh
 #!/bin/bash
 
+# ls..
 echo 'alias vrimm="ls"' >> ~/.bashrc
 echo 'alias vrimm-tutt-cos="ls -la"' >> ~/.bashrc
 
+# cd..
 echo 'alias tras="cd"' >> ~/.bashrc
 echo 'alias aret="cd .."' >> ~/.bashrc
 
-echo 'alias accirt="shutdown"' >> ~/.bashrc
-echo 'alias buonanotte="shutdown"' >> ~/.bashrc
-echo 'alias stutall="shutdown"' >> ~/.bashrc
+# shutdown
+echo 'alias accirt="shutdown -h now"' >> ~/.bashrc
+echo 'alias buonanotte="shutdown -h now"' >> ~/.bashrc
+echo 'alias stutall="shutdown -h now"' >> ~/.bashrc
+
+# update
+echo 'alias scarica="sudo apt update"' >> ~/.bashrc
+echo 'alias aggiorn="sudo apt upgrade"' >> ~/.bashrc
+echo 'alias aggiorn-tutt-cos="sudo apt dist-upgrade"' >> ~/.bashrc
+echo 'alias cancell="sudo apt autoremove"' >> ~/.bashrc
+echo 'alias pulezz="sudo apt-get clean"' >> ~/.bashrc
+echo 'alias pulezz-tutt-cos="sudo apt-get autoclean"' >> ~/.bashrc
 
 source ~/.bashrc
