d5c3feda6e6ed210c60fd810663b84e2445d369b
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-05-11 22:16:45 +0200

    pipe esercizo2

--- pipe/Esercizio2.c
+++ pipe/Esercizio2.c
@@ -1,38 +1,95 @@
-/*Esercizio: scrivere un programma che esegua il comando di
-shell “ls | sort| grep <pat>” con tre processi distinti*/
-
+/**
+ * Scrivere un programma che esegua il comando di shell 
+ * “ls | sort| grep <path>” con tre processi distinti
+*/
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+// direttive del processore
+#define READ 0
+#define WRITE 1
 
 
-int main(int argc, char** argv){
-    int pfd1[2], pfd2[2], pid;
+int main( int argc, char** argv ) {
 
-    if(pipe(pfd1) == -1){
-        perror("error creating pipe");
-        return -1;
+    // utilizzo 4 descrittori
+    int pfdPipe1[2];
+    int pfdPipe2[2];
+    int pid;
+
+    // creo le pipe effettuando dei controlli
+    if( pipe( pfdPipe1 ) == -1 ) {
+        perror( "pipe" );
+        exit( EXIT_FAILURE );
     }
 
-        if(pipe(pfd2) == -1){
-        perror("error creating pipe");
-        return -1;
+    if( pipe( pfdPipe2 ) == -1 ) {
+        perror( "pipe" );
+        exit( EXIT_FAILURE );
     }
 
-    if(!(pid = fork())){
-        close(pfd1[0]);
-        dup2(pfd1[1], 1);
-        close(pfd1[1]);
-        execlp("ls", "ls", NULL);
-    } else{
-        close(pfd1[1]);
-        dup2(pfd1[0], 0);
-        close(pfd1[0]);
-        execlp("sort", "sort", "-r", NULL);
-        if(pid = fork()){
-            close(pfd2[1]);
-            dup2(pfd2[0], 0);
-            close(pfd2[0]);
-            execlp("grep", "grep", argv[1], NULL);
+    if ( ( pid = fork() ) == -1 ) {
+        perror( "fork" );
+        exit( EXIT_FAILURE );
+    }
+
+    if ( pid != 0 ) { // padre
+
+        // chiudo i descrittori che non utilizzo
+        close( pfdPipe1[ READ ] );
+        close( pfdPipe2[ READ ] );
+        close( pfdPipe2[ WRITE ] );
+
+        // duplico il file descrittore di scrittura 
+        dup2( pfdPipe1[ WRITE ], WRITE );
+
+        // chiudo il descrittore utilizzato
+        close( pfdPipe1[ WRITE ] );
+
+        // eseguo il comando shell
+        execlp( "ls", "ls", NULL );
+
+    } else { // figlio
+
+        // creo un figlio del processo
+        if ( ( pid = fork() ) == -1 ) {
+            perror( "fork" );
+            exit( EXIT_FAILURE );
+        }
+
+        if ( !( pid != 0 ) ) { // padre
+
+            // chiudo i descrittori che non utilizzo
+            close( pfdPipe1[ WRITE ] );
+            close( pfdPipe2[ READ ] );
+
+            // leggo e scrivo
+            dup2( pfdPipe1[ READ ], READ );
+            dup2( pfdPipe2[ WRITE ], WRITE );
+
+            // chiudo
+            close( pfdPipe1[ READ ] );
+            close( pfdPipe2[ WRITE ] );
+
+            // eseguo
+            execlp( "sort", "sort", "-r", NULL );
+
+        } else { // figlio
+
+            // chiudo i descrittori che non utilizzo
+            close( pfdPipe1[ READ ] );
+            close( pfdPipe1[ WRITE ] );
+            close( pfdPipe2[ WRITE ] );
+
+            // leggo
+            dup2( pfdPipe2[ READ ], READ );
+
+            // chiudo
+            close( pfdPipe2[ READ ] );
+
+            // eseguo
+            execlp( "grep", "grep", argv[1], NULL );
         }
     }
 
