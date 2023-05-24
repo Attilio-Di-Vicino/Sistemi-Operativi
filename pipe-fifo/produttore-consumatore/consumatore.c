c1783cac78c1f752c12e2d6c547a7d3e503c7096
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-05-24 16:56:48 +0200

    produttore-consumatore fifo

--- pipe-fifo/produttore-consumatore/consumatore.c
+++ pipe-fifo/produttore-consumatore/consumatore.c
@@ -6,35 +6,45 @@
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 #define FIFO_NAME "/tmp/my_fifo"
 #define BUFFER_SIZE 100 // PIPE_BUF
 
+/**
+ * Proecsso consumatore aperto in lettura
+*/
 int main() {
 
     int pipe_fd;
     int res;
     int open_mode = O_RDONLY;
-    char buffer[BUFFER_SIZE];
+    char buffer[ BUFFER_SIZE ];
     int bytes_read = 0;
     
-    printf("Process %d opening FIFO O_RDONLY\n", getpid());
+    printf( "Process %d opening FIFO O_RDONLY\n", getpid() );
     
-    pipe_fd = open(FIFO_NAME, open_mode);
+    // apertura pipe creata precedentemente
+    // e ritorna il descrittore della pipe con nome
+    // se ritorna -1 c'è un errore
+    pipe_fd = open( FIFO_NAME, open_mode );
     
-    printf("Process %d result %d\n", getpid(), pipe_fd);
+    printf( "Process %d result %d\n", getpid(), pipe_fd );
     
-    if (pipe_fd != -1) {
+    // se non ci sono stati errori nell'apertura della pipe
+    if ( pipe_fd != -1 ) {
         
+        // leggi dalla pipe
         do {
-            res = read(pipe_fd, buffer, BUFFER_SIZE);
+            res = read( pipe_fd, buffer, BUFFER_SIZE );
             bytes_read += res;
-        } while (res > 0);
+        } while ( res > 0 );
 
-        close(pipe_fd);
+        // chiusura pipe
+        close( pipe_fd );
     } else {
         exit(-1);
     }
     
-    printf("Process %d finished,%d bytes read\n",getpid(),bytes_read);
-    exit(0);
+    printf( "Process %d finished,%d bytes read\n", getpid(), bytes_read );
+    exit( EXIT_SUCCESS );
 }
--- pipe-fifo/produttore-consumatore/produttore.c
+++ pipe-fifo/produttore-consumatore/produttore.c
@@ -15,8 +15,11 @@
 
 #define FIFO_NAME "/tmp/my_fifo"
 #define BUFFER_SIZE 100 // PIPE_BUF
-#define TEN_MEG (1024 * 1000 * 10)
+#define TEN_MEG ( 1024 * 1000 * 10 )
 
+/**
+ * Processo produttore
+*/
 int main() {
 
     int pipe_fd;
@@ -25,42 +28,48 @@ int main() {
     int bytes_sent = 0;
     char buffer[ BUFFER_SIZE ];
 
-    if (access(FIFO_NAME, F_OK) == -1) {
+    // Se la FIFO non esiste la creiamo. Poi viene aperta
+    // questa condizione controlla se il file FIFO non esiste o non può essere letto
+    // in tal caso la crea
+    if ( access( FIFO_NAME, F_OK ) == -1 ) {
         
-        res = mkfifo(FIFO_NAME, 0777);
+        // creao la fifo con i permessi
+        res = mkfifo( FIFO_NAME, 0777 );
         
-        if (res != 0) {
-            printf("Could not create fifo %s\n", FIFO_NAME);
+        if ( res != 0 ) {
+            printf( "Could not create fifo %s\n", FIFO_NAME );
             exit(-1);
         }
 
-        printf("Process %d opening FIFO O_WRONLY\n", getpid());
+        printf( "Process %d opening FIFO O_WRONLY\n", getpid() );
 
-        pipe_fd = open(FIFO_NAME, open_mode);
+        // apro la fifo 
+        pipe_fd = open( FIFO_NAME, open_mode );
 
-        printf("Process %d result %d\n", getpid(), pipe_fd);
+        printf( "Process %d result %d\n", getpid(), pipe_fd );
 
-        if (pipe_fd != -1) {
+        // se la fifo si è aperta correttamente
+        if ( pipe_fd != -1 ) {
             
-            while(bytes_sent < TEN_MEG) {
+            while( bytes_sent < TEN_MEG ) {
                 
                 // supponiamo che buffer sia riempito altrove
-                res = write(pipe_fd, buffer, BUFFER_SIZE);
+                res = write( pipe_fd, buffer, BUFFER_SIZE );
                 
-                if (res == -1) {
-                    printf("Write error on pipe\n");
-                    exit(1);
+                if ( res == -1 ) {
+                    printf( "Write error on pipe\n" );
+                    exit( EXIT_FAILURE );
                 }
                 
                 bytes_sent += res;
             }
             
-            close(pipe_fd);
+            close( pipe_fd );
         } else {
-            exit(1);
+            exit( EXIT_FAILURE );
         }
         
-        printf("Process %d finished\n", getpid());
-        exit(1);
+        printf( "Process %d finished\n", getpid() );
+        exit( EXIT_FAILURE );
     }
 }
