5a784cd8d7dd366bb5331ae8c7fb62264abe0779
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-02 12:29:11 +0200

    esercizio4

--- 13-esercitazioni/esercizio4.c
+++ 13-esercitazioni/esercizio4.c
@@ -11,7 +11,9 @@
  * • puntatore al primo elemento occupato: readpos
  * 
  * Il produttore, inserisce, 20 numeri interi in maniera sequenziale.
+ * 
  * Il consumatore li estrae sequenzialmente per stamparli.
+ * 
  * Il programma dovrà prevedere:
  * 
  * • un meccanismo di accesso controllato alla risorsa buffer da parte dei due Thread
@@ -25,3 +27,123 @@
  *      -> buffer vuoto: definizione di una condition per la sospensione del
  *         consumatore se il buffer è vuoto ( notempty )
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+#define SIZE 20
+#define MAXVAL 100
+#define MAX_ITER 46
+#define OVER -1
+
+// Struttura condivisa
+struct buffer {
+    int count; // contatore elementi contenuti
+    int writepos; // prima posizione disponibile
+    int readpos; // posizione primo elemento disponibile
+    int* myBuffer;
+};
+
+void printBuffer( struct buffer* myBuffer ) {
+    printf( "\nNel buffer ci sono %d elementi", myBuffer->count );
+    printf( "\nLa prima posizione disponibile è %d", myBuffer->writepos % SIZE );
+    printf( "\nIl primo elemento disponibile si trova nella posizione %d", myBuffer->readpos % SIZE );
+}
+
+void printMyBuffer( struct buffer* sharedBuffer ) {
+    printf( "\n\nBuffer: " );
+    for ( int i = 0; i < SIZE; i++ )
+        printf( "%d ", sharedBuffer->myBuffer[i] );
+    printf( "\n" );
+}
+
+// Mutex e Var cond inizializzate staticamente
+struct sync {
+    pthread_mutex_t mutex;
+    pthread_cond_t notfull;
+    pthread_cond_t notempty;
+} shared = { PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER };
+
+void* produce( void* argv ) {
+    struct buffer* sharedBuffer = ( struct buffer* ) argv;
+
+    // Impongo un limite alle iterazioni
+    for ( int i = 0; i < MAX_ITER; i++ ) {
+        while ( sharedBuffer->count >= SIZE )
+            pthread_cond_wait( &shared.notfull, &shared.mutex );
+        // Sezione critica
+        int val = rand() % MAXVAL + 1;
+        sharedBuffer->myBuffer[ sharedBuffer->writepos % SIZE ] = val;
+        printf( "\nThread produttore produce il valore [%d] alla posizione [%d]", val, sharedBuffer->writepos % SIZE );
+        sharedBuffer->writepos++;
+        sharedBuffer->count++;
+        pthread_cond_signal( &shared.notempty );
+        pthread_mutex_unlock( &shared.mutex );
+    }
+    pthread_exit( EXIT_SUCCESS );
+}
+
+void* consume( void* argv ) {
+    struct buffer* sharedBuffer = ( struct buffer* ) argv;
+
+    // Impongo un limite alle iterazioni
+    for ( int i = 0; i < MAX_ITER; i++ ) {
+        while ( sharedBuffer->count == 0 )
+            pthread_cond_wait( &shared.notempty, &shared.mutex );
+        // Sezione critica
+        int val = sharedBuffer->myBuffer[ sharedBuffer->readpos % SIZE ];
+        sharedBuffer->myBuffer[ sharedBuffer->readpos % SIZE ] = - 1;
+        printf( "\nThread consumatore consuma il valore [%d] alla posizione [%d]", val, sharedBuffer->readpos % SIZE );
+        sharedBuffer->readpos++;
+        sharedBuffer->count--;
+        pthread_cond_signal( &shared.notfull );
+        pthread_mutex_unlock( &shared.mutex );
+    }
+    pthread_exit( EXIT_SUCCESS );
+}
+
+void main() {
+
+    // Allocazione e inizializzazione buffer condiviso
+    struct buffer* sharedBuffer = ( struct buffer* ) malloc( sizeof( struct buffer ) );
+    sharedBuffer->count = 0;
+    sharedBuffer->readpos = 0;
+    sharedBuffer->writepos = 0;
+    sharedBuffer->myBuffer = ( int* ) calloc( SIZE, sizeof( int ) );
+    for ( int i = 0; i < SIZE; i++ )
+        sharedBuffer->myBuffer[i] = OVER;
+
+    // Thread
+    pthread_t produttore, consumatore;
+
+    // Creazione Thread produttore
+    if ( pthread_create( &produttore, NULL, produce, ( void* ) sharedBuffer ) != 0 ) {
+        perror( "Errore nella creazione del Thread Produttore" );
+        exit( EXIT_FAILURE );
+    }
+
+    // Creazione Thread consumatore
+    if ( pthread_create( &consumatore, NULL, consume, ( void* ) sharedBuffer ) != 0 ) {
+        perror( "Errore nella creazione del Thread Consumatore" );
+        exit( EXIT_FAILURE );
+    }
+
+    // Attesa Thread produttore
+    if ( pthread_join( produttore, NULL ) != 0 ) {
+        perror( "Errore nella join del Thread Produttore" );
+        exit( EXIT_FAILURE );
+    }
+
+    // Attesa Thread consumatore
+    if ( pthread_join( consumatore, NULL ) != 0 ) {
+        perror( "Errore nella join del Thread Consumatore" );
+        exit( EXIT_FAILURE );
+    }
+
+    printMyBuffer( sharedBuffer );
+    printBuffer( sharedBuffer );
+    printf( "\n" );
+    exit( EXIT_SUCCESS );
+}
