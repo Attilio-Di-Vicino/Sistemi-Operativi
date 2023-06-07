8d9f00984da2c9e95ae49ac2d9282edcb1ececb7
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-07 16:56:23 +0200

    traccie esercizi

---
+++ 15-esercitazioni/esercizio4.c
@@ -0,0 +1,13 @@
+/**
+ * ESERCIZIO: 4
+ * 
+ * Si realizzi un programma C e Posix in ambiente Linux che, impiegando la libreria Pthread,  
+ * crei una matrice di interi di dimensioni nxn con n numero dispari fornito da riga di comando,  
+ * che provveda a sommare in modo concorrente con quattro thread: 
+ * 
+ *  1. gli elementi delle due diagonali
+ *  2. gli elementi della riga centrale
+ *  3. gli elementi della colonna centrale
+ * 
+ * e ne determini il massimo da assegnare ad un'opportuna variabile. 
+*/
---
+++ 15-esercitazioni/esercizio5.c
@@ -0,0 +1,10 @@
+/**
+ * ESERCIZIO: 5
+ * 
+ * Si realizzi un programma C e Posix in ambiente Linux che, impiegando la libreria Pthread, 
+ * crei un vettore di interi di dimensione n, dove n>=24 e multiplo di 4, è un argomento passato da riga di comando.
+ *   
+ * Calcolati i valori medi degli elementi contenuti nei quattro sottovettori di taglia n/4,  
+ * il programma deve determinare, impiegando quattro thread,
+ * la somma del numero di elementi minori del valore medio in ciascun sottovettore.
+*/
