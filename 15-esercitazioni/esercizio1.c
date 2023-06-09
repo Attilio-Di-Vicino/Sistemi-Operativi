5f9956cf62de6972d01842cd298bb22d507abec6
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-09 19:06:43 +0200

    es

--- 15-esercitazioni/esercizio1.c
+++ 15-esercitazioni/esercizio1.c
@@ -17,25 +17,6 @@
  * attende 5 secondi e termina.
  */
 
-/**
- * ESERCIZIO: 3
- * 
- * Facendo uso della libreria Pthread( fatto con i semafori per esercizio ),
- * 
- * realizzare un programma in cui un thread scrittore,
- * dato un intero N da riga di comando ( dove 10 < N <= 15 ),
- * scrive in un file nella prima posizione,
- * 
- * uno alla volta ed ogni ½ secondo, la sequenza di Fibonacci di ordine N, 
- * alternandosi con un thread lettore che legge, uno alla volta dalla
- * prima posizione del file, i numeri scritti dal thread scrittore.
- * 
- * Un terzo thread attende la lettura dell’ N-esimo intero, quindi
- * stampa a video il messaggio 
- * “Operazioni concluse, arrivederci dal thread: tid”,
- * attende 5 secondi e termina.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
