67d2d6084cbc4ac6819cd66da278c5ffc2a9be0e
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-10 16:25:21 +0200

    esercizio #byAttilio

--- 16-esercizi-vari/esercizio5.c
+++ 16-esercizi-vari/esercizio5.c
@@ -2,7 +2,8 @@
  * ESERCIZIO: 5
  * 
  * Si realizzi un programma C e Posix in ambiente Linux che, impiegando la libreria Pthread, 
- * crei un vettore di interi di dimensione n, dove n>=24 e multiplo di 4, è un argomento passato da riga di comando.
+ * crei un vettore di interi di dimensione n, dove n >= 24 e multiplo di 4,
+ * è un argomento passato da riga di comando.
  *   
  * Calcolati i valori medi degli elementi contenuti nei quattro sottovettori di taglia n/4,  
  * il programma deve determinare, impiegando quattro thread,
