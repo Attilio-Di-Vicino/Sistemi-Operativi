d60657acec36706a682582df15fb10ed43a7229b
Attilio-Di-Vicino <attilio.divicino001@studenti.uniparthenope.it>
2023-06-12 20:37:18 +0200

    thread esame

--- thread-esame/prova-scritta-24-06-2009.c
+++ thread-esame/prova-scritta-24-06-2009.c
@@ -7,7 +7,8 @@
  * 
  * Realizzare un programma C e Posix sotto Linux che sincronizzi
  * un processo padre ed un processo figlio / un thread scrittore e uno lettore
- * che scrivono e leggono, rispettivamente, un numero intero alla volta (da 1 a n, dove n è passato da riga di  comando)
+ * che scrivono e leggono, rispettivamente, un numero intero alla volta
+ * (da 1 a n, dove n è passato da riga di  comando)
  * nella  prima  posizione  di  un  file  temporaneo  opportunamente  creato. 
  * 
  * Implementare  il  programma con i processi ed i semafori Posix.
